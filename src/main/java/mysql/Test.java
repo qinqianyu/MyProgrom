da801a1ee4a67ac5c83d3cf59bc8865e80e5d520
jiaxiangkai <2410992642@qq.com>
2019-10-29 09:48:33 +0800

    10.29.01

--- src/main/java/mysql/Test.java
+++ src/main/java/mysql/Test.java
@@ -3,26 +3,88 @@ package mysql;
 import com.alibaba.fastjson.JSON;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import static org.neo4j.driver.v1.Values.parameters;
+
 public class Test {
+    private final Map<String, Integer> map;
+
+    {
+        map = new HashMap<>();
+        map.put("个人合伙执照", 2);
+        map.put("个人独资执照", 2);
+        map.put("个体工商执照", 2);
+        map.put("中华人民共和国军官证", 1);
+        map.put("中华人民共和国居民身份证", 1);
+        map.put("中华人民共和国警官证", 1);
+        map.put("公司执照", 2);
+        map.put("其他类型", 0);
+        map.put("其它有效身份证件", 1);
+        map.put("分公司执照", 2);
+        map.put("外商承包企业执照", 2);
+        map.put("外商投资企业执照", 2);
+        map.put("外国（地区）护照", 1);
+        map.put("居民证", 1);
+        map.put("澳门居民身份证", 1);
+        map.put("营业执照", 2);
+        map.put("通行证", 1);
+        map.put("集团登记证", 2);
+        map.put("非公司制企业法人执照", 2);
+        map.put("香港居民身份证", 1);
+    }
+
     public static void main(String[] args) {
+        String split[] = {"mid_mkt_stkhldr",
+                "500104100002438",
+                "其他投资者",
+                "重庆市九龙坡区八桥乡",
+                "十八治机电公司",
+                "*",
+                "其他类型"};
+        if (split.length < 6 || split[1].length() < 6 || split[4].length() < 6 || split[4].endsWith("*")) {
+            System.out.println("555");
+        }
+        switch (new Test().Judge(split[5], split[2])) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println(2);
+        }
+    }
+
+    int Judge(String str, String str1) {
+        if (map.get(str) == 1) {
+            return 1;
+        } else if (map.get(str) == 2) {
+            return 2;
+        }
+        if (str1.equals("自然人")) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main1(String[] args) {
         Random rand = new Random();
         int a;
-        for (int j = 0; j <10 ; j++) {
+        for (int j = 0; j < 10; j++) {
             a = rand.nextInt(1);
         }
-        Guanxi guanxi = new Guanxi(1,2,3,4,5,"s1","s2","s3","s4","s5");
+        Guanxi guanxi = new Guanxi(1, 2, 3, 4, 5, "s1", "s2", "s3", "s4", "s5");
         Map<String, Object> map = entityToMap(guanxi);
         String s = JSON.toJSONString(map);
         System.out.println(s);
         System.out.println(map.toString());
     }
+
     public static Map<String, Object> entityToMap(Object object) {
         Map<String, Object> map = new HashMap();
-        for (Field field : object.getClass().getDeclaredFields()){
+        for (Field field : object.getClass().getDeclaredFields()) {
             try {
                 boolean flag = field.isAccessible();
                 field.setAccessible(true);
--- src/main/java/untils/JiSuanQi.java
+++ src/main/java/untils/JiSuanQi.java
@@ -2,8 +2,10 @@ package untils;
 
 public class JiSuanQi {
     public static void main(String[] args) {
-        Long a=(5372847/(2384405-1606836L));
+        Long a=((
+        1572232823207L-1572232326569L
+)/1000/60);
         String a1 = "";
-        System.out.println(a1.length());
+        System.out.println(a);
     }
 }
